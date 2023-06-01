587beba79a7209456e597b4394b96ab4490742f0
s_Gdevelopers <s_Gdevelopers@amplifon.com>
2023-06-01 17:37:23 +0200

    init

--- restBot.py
+++ restBot.py
@@ -1,28 +1,12 @@
-from binance.spot import Spot
+import asyncio
+from binance import AsyncClient, BinanceSocketManager
+import pandas as pd
+import sqlalchemy
+import secret
+import nest_asyncio
 
-client = Spot()
-# Get server timestamp
-print(client.time())
-# Get klines of BTCUSDT at 1m interval
-print(client.klines("BTCUSDT", "1m"))
-# Get last 10 klines of BNBUSDT at 1h interval
-print(client.klines("BNBUSDT", "1h", limit=10))
+nest_asyncio.apply()
 
-# api key/secret are required for user data endpoints
-client = Spot(api_key='', api_secret='')
+symbol = 'BTC/USDT'
 
-# Get account and balance information
-print(client.account())
-
-# Post a new order
-params = {
-    'symbol': 'BTCUSDT',
-    'side': 'SELL',
-    'type': 'LIMIT',
-    'timeInForce': 'GTC',
-    'quantity': 0.002,
-    'price': 9500
-}
-
-response = client.new_order(**params)
-print(response)
+engine = sqlalchemy.create_engine('sqlite:///'+symbol+'stream.db')
