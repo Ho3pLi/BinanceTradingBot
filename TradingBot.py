a544fc2e16773b82050c9a64dc5b2489c9a20b2b
Ho3pLi <daniele.barile0101@gmail.com>
2023-06-03 11:41:03 +0200

    TradingBot init

---
+++ TradingBot.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+import pandas as pd
+from binance.client import Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+api_secret = os.getenv("API_SECRET")
+
+client = Client(api_key=api_key, api_secret=api_secret)
+symbol = 'BTCUSDT'
+engine = sqlalchemy.create_engine('sqlite:///'+symbol+'stream.db')
+df = pd.read_sql(symbol, engine)
+print(df)
